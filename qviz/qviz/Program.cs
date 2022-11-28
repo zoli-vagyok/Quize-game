a34a9d25f52bb412d1ffb007354972d7c2643fdd
Lufcsik Zoltan <lufcsik_zoltan@mathiasz.local>
2022-11-28 10:13:59 +0100

    1128

--- qviz/qviz/Program.cs
+++ qviz/qviz/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,82 @@ namespace qviz
 {
     internal class Program
     {
+        static List<string> kerdeslist = new List<string>();
+
+        static int kerdesbeolvas()
+        {
+            StreamReader kerdesbe = new StreamReader("Kerdesek.txt");
+            while (!kerdesbe.EndOfStream)
+            {
+                kerdeslist.Add(kerdesbe.ReadLine());
+            }
+            kerdesbe.Close();
+            return kerdeslist.Count();
+        }
+        static List<string> valaszteklist = new List<string>();
+
+        static int valasztekbeolvas()
+        {
+            StreamReader valasztekbe = new StreamReader("valasztek.txt");
+            while (!valasztekbe.EndOfStream)
+            {
+                valaszteklist.Add(valasztekbe.ReadLine());
+            }
+            valasztekbe.Close();
+            return valaszteklist.Count();
+        }
+        static List<string> valaszoklist = new List<string>();
+
+        static int valaszokbeolvas()
+        {
+            StreamReader valaszokbe = new StreamReader("valaszok.txt");
+            while (!valaszokbe.EndOfStream)
+            {
+                valaszteklist.Add(valaszokbe.ReadLine());
+            }
+            valaszokbe.Close();
+            return valaszoklist.Count();
+        }
+        static int szamol()
+        {
+            int szamol = 0;
+            for (int i = 0; i < kerdeslist.Count(); i++)
+            {
+                szamol++;
+            }
+            return szamol;           
+        }
+        static int kesz()
+        { 
+            if (szamol == kerdeslist.Count())
+            {
+
+            }
+            return vege;
+        }
         static void Main(string[] args)
         {
+            for (int i = 0; i < kerdeslist.Count(); i++)
+            {
+                Console.WriteLine(kerdeslist[i]);
+                Console.WriteLine(valaszteklist[i]);
+                Console.WriteLine("Add meg a válasz betüjelét");
+                string valasz = Console.ReadLine();
+
+                while (valasz != "a" && valasz != "b" && valasz != "c" && valasz != "d")
+                {
+                    Console.WriteLine("a, b, c, vagy d-t írj óda!!");
+                    Console.WriteLine(kerdeslist[i]);
+                    Console.WriteLine(valaszteklist[i]);
+                    Console.WriteLine("Add meg a válasz betüjelét");
+                    valasz = Console.ReadLine();
+                }
+                if (valasz != valaszoklist[i])
+                {
+                    Console.WriteLine("Rossz a válasz, kezd előről");
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
